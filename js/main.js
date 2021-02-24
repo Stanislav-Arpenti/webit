aff3f3b9712374c48dbd52132ebe6617be01c472
Stanislav-Arpenti <77639330+Stanislav-Arpenti@users.noreply.github.com>
2021-02-25 01:55:58 +0200

    webit-update

--- js/main.js
+++ js/main.js
@@ -3,25 +3,51 @@ $(document).ready(function(){
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		infinite: true,
+		prevArrow: $('.slider-prev'),
+		nextArrow: $('.slider-next'),
 		responsive: [
 		{
-			breakpoint: 768,
+			breakpoint: 992,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1,
 			}
 		},
 		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				autoplay:true,
+				arrows:false,
+			}
+		},
+		{
 			breakpoint: 576,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1,
+				arrows:false,
 			}
-		}
+		},
+
 		],
 
 	});
 
+	$('.gallery_video').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+/*		prevArrow: $('.gallery-prev'),
+		nextArrow: $('.gallery-next'),*/
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows:false,
+			}
+		},
+		],
+	});
 
 	$('.header_burger').click(function(event){
 		$('.header_burger,.mobile_menu').toggleClass('active');
